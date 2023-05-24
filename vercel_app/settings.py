00729dd6f8219732f1c20d88eb76e770471bf9c9
Sebastian G <seba.gallardog@duocuc.cl>
2023-05-23 23:13:22 -0400

    fix css 4

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -1,8 +1,11 @@
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
@@ -99,6 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 
 # Default primary key field type
