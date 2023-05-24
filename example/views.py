c45b9da2272dc484b5ea548649366cc3fef4cc2f
Sebastian G <seba.gallardog@duocuc.cl>
2023-05-23 22:41:36 -0400

    xd

--- example/views.py
+++ example/views.py
@@ -1,16 +1,5 @@
-# example/views.py
-from datetime import datetime
+from django.shortcuts import render
 
-from django.http import HttpResponse
 
-def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+def Index(request):
+    return render(request, 'index.html')
